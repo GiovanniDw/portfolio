f33fb625b5fe0a1ccfdde5c854258cffce27afec
GiovanniDw <giovannidwidjosewojo@hotmail.com>
2020-08-14 23:56:38 +0200

    link hover

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
+import { motion } from 'framer-motion'
 
 export default class About extends Component {
     render() {
         return (
-			<section id='About' className='about'>
-				<h1>
+			<motion.section id='About' className='about'>
+				<motion.h1>
 					Hi <span role='img'>👋</span>, <br></br>My name is{' '}
-					<span className='my-name'>Giovanni</span>,<br></br> I’m a CMD student
-				</h1>
-			</section>
+					<motion.span
+						className='my-name'
+					>
+						Giovanni
+					</motion.span>
+					,<br></br> I’m a CMD student
+				</motion.h1>
+			</motion.section>
 		);
     }
 }
