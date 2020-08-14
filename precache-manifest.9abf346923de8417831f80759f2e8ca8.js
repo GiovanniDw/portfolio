f3e6db7cb0020617fa8c82ab6227a840ccaf1e1a
GiovanniDw <giovannidwidjosewojo@hotmail.com>
2020-08-14 13:57:57 +0200

    Updates

---
+++ precache-manifest.9abf346923de8417831f80759f2e8ca8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c316eea5a38797570f7f7830d5c4df",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4e630be052d40055984a",
+    "url": "/portfolio/static/css/main.1413795b.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e630be052d40055984a",
+    "url": "/portfolio/static/js/main.214260e0.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
